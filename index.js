8532f9689edf0713535740dd07509bb2a8abe4c1
Pawan M <pawanmurthy92@gmail.com>
2018-02-06 03:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ var recipe={
   
   
 }
+
+var obj = { prop: 1 }
+ function updateObjectWithKeyAndValue(object, key, value){
+   return Object.assign({}, obj, {[key]: value})
+ }
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+   object[key] = value
+   return object
+ }
+ var newObj = Object.assign({}, obj);
+ function deleteFromObjectByKey(object, key){
+   delete newObj[key];
+   return newObj
+ }
+ function destructivelyDeleteFromObjectByKey(object, key){
+   delete object[key]
+   return object
+ }
